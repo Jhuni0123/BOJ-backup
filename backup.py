f2d88a9d2a51574d0fcbf02b6edaa839a7869c26
Jhuni0123 <whdgnsdl887@naver.com>
2018-01-24 19:29:44 +0900

    Handle errors

--- backup.py
+++ backup.py
@@ -3,6 +3,7 @@ from getpass import getpass
 import os
 import argparse
 import textwrap
+import requests
 
 
 def process_argument():
@@ -35,30 +36,36 @@ if __name__ == '__main__':
     login_id = input('BOJ login_id: ')
     password = getpass('BOJ password: ')
 
-    success = myboj.login(login_id, password)
-
-    if not success:
-        print('error: invalid id or password')
-        exit(1)
-
-    submission_list = myboj.get_submission_list(args.result, args.limit)
-
-    # backup
-    dirname = 'BOJ-' + myboj.user_id
-
     try:
-        os.mkdir(dirname, 0o755)
-    except FileExistsError:
-        pass
+        success = myboj.login(login_id, password)
 
-    print("Backing-up in '%s/'" % dirname)
+        if not success:
+            print('error: invalid id or password')
+            exit(1)
 
-    for idx, submit in enumerate(submission_list):
-        submit = {**myboj.get_submission_info(submit['submission_id']), **submit}
+        submission_list = myboj.get_submission_list(args.result, args.limit)
 
-        filename = '%s-%s-%s.%s' % (submit['submission_id'], submit['problem_id'], submit['lang'], submit['ext'])
-        f = open(os.path.join(dirname, filename), 'w')
-        f.write(submit['sourcecode'])
-        f.close()
-        print("\r(%d/%d) %35s saved." % (idx+1, len(submission_list), filename), end='')
-    print('\nDone!')
+        # backup
+        dirname = 'BOJ-' + myboj.user_id
+
+        try:
+            os.mkdir(dirname, 0o755)
+        except FileExistsError:
+            pass
+
+        print("Backing-up in '%s/'" % dirname)
+
+        for idx, submit in enumerate(submission_list):
+            submit = {**myboj.get_submission_info(submit['submission_id']), **submit}
+
+            filename = '%s-%s-%s.%s' % (submit['submission_id'], submit['problem_id'], submit['lang'], submit['ext'])
+            f = open(os.path.join(dirname, filename), 'w')
+            f.write(submit['sourcecode'])
+            f.close()
+            print("\r(%d/%d) %35s saved." % (idx+1, len(submission_list), filename), end='')
+        print('\nDone!')
+    except requests.exceptions.ConnectionError:
+        print('error: network connection is not reliable')
+    except Exception as e:
+        print(e)
+        print('error: unexpected error occurd. The structure of boj pages may changed.')
