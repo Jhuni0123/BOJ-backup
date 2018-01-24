8fa00f2748cbd10d1d00191b2ddce55ca2817c8f
Jhuni0123 <whdgnsdl887@naver.com>
2018-01-24 22:29:06 +0900

    Make backup function

--- backup.py
+++ backup.py
@@ -27,6 +27,28 @@ def process_argument():
     return parser.parse_args()
 
 
+def backup(boj, args):
+    submission_list = boj.get_submission_list(args.result, args.limit)
+
+    dirname = 'BOJ-' + boj.user_id
+    try:
+        os.mkdir(dirname, 0o755)
+    except FileExistsError:
+        pass
+
+    print("Backing-up in '%s/'" % dirname)
+
+    for idx, submit in enumerate(submission_list):
+        submit = {**boj.get_submission_info(submit['submission_id']), **submit}
+
+        filename = '%s-%s-%s.%s' % (submit['submission_id'], submit['problem_id'], submit['lang'], submit['ext'])
+        f = open(os.path.join(dirname, filename), 'w')
+        f.write(submit['sourcecode'])
+        f.close()
+        print("\r(%d/%d) %35s saved." % (idx+1, len(submission_list), filename), end='')
+    print('\nDone!')
+
+
 if __name__ == '__main__':
     args = process_argument()
 
@@ -43,27 +65,8 @@ if __name__ == '__main__':
             print('error: invalid id or password')
             exit(1)
 
-        submission_list = myboj.get_submission_list(args.result, args.limit)
+        backup(myboj, args)
 
-        # backup
-        dirname = 'BOJ-' + myboj.user_id
-
-        try:
-            os.mkdir(dirname, 0o755)
-        except FileExistsError:
-            pass
-
-        print("Backing-up in '%s/'" % dirname)
-
-        for idx, submit in enumerate(submission_list):
-            submit = {**myboj.get_submission_info(submit['submission_id']), **submit}
-
-            filename = '%s-%s-%s.%s' % (submit['submission_id'], submit['problem_id'], submit['lang'], submit['ext'])
-            f = open(os.path.join(dirname, filename), 'w')
-            f.write(submit['sourcecode'])
-            f.close()
-            print("\r(%d/%d) %35s saved." % (idx+1, len(submission_list), filename), end='')
-        print('\nDone!')
     except requests.exceptions.ConnectionError:
         print('error: network connection is not reliable')
     except Exception as e:
